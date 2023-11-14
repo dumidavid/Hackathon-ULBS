771b78ed0d93f2896554dc7b2ac64ba761964b07
SacomaSack <126544793+EduardAntonescu@users.noreply.github.com>
2023-11-14 16:37:48 +0200

    Added lanes and cars random

---
+++ SemaforSmart.py
@@ -0,0 +1,13 @@
+import random
+
+
+class TrafficLight:
+    def __innit__(self, lanes):
+        self.lanes = {lane: [] for lane in lanes}
+
+
+def measure_traffic(self):
+    for lane in self.lanes:
+        cars_detected = random.randint(0, 10)
+        distances = [random.randint(1, 200) for _ in range(cars_detected)]
+        self.lanes[lane] = distances
