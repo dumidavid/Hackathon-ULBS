4c07d1c8c3c9be3360335a5c5d7c7859255d2b35
SacomaSack <126544793+EduardAntonescu@users.noreply.github.com>
2023-11-15 18:16:52 +0200

    adding traffic light based on prioritisation

--- SemaforSmart.py
+++ SemaforSmart.py
@@ -1,9 +1,10 @@
 import random
-
+import time
 
 class TrafficLight:
     def __init__(self, lanes):
         self.lanes = {lane: [] for lane in lanes}
+        self.green_lane = None
 
     def measure_traffic(self):
         for lane in self.lanes:
@@ -22,10 +23,22 @@ class TrafficLight:
         priority1, priority2 = zip(*sorted(zip(temppriority[1], temppriority[0])))
         return list(priority2)
 
+    def traffic_lights_on_prioritisation(self):
+        prioritized_lanes = self.prioritisation()
+        if self.green_lane:
+            print(f"Red light for {self.green_lane} lane.")
+            time.sleep(2)
+        self.green_lane = prioritized_lanes[0]
+        print(f"Green light for {self.green_lane} lane.")
+        time.sleep(5)
+
+    def run_traffic_lights(self, duration):
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            self.traffic_lights_on_prioritisation()
+
 
 if __name__ == '__main__':
     lanes = ["Lane1", "Lane2", "Lane3", "Lane4"]
     traffic_light = TrafficLight(lanes)
-    a = traffic_light.prioritisation()
-
-    print(a)
+    traffic_light.run_traffic_lights(30)
